96489ef3d2f2934ebf9994b09b1b459d3fa0ccf8
Frank Ackermann <acki77.github@digital-filestore.de>
2026-03-11 18:21:33 +0100

    happypath-test and second error-test finished

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -4,26 +4,45 @@ import Player from "@/models/Player";
 
 describe("Player Model Test", () => {
   it("should fail if no name is provided", async () => {
-    console.log(
-      "Pflichtfelder:",
-      Object.keys(Player.schema.paths).filter(
-        (p) => Player.schema.paths[p].isRequired,
-      ),
-    );
     const playerWithoutFirstName = new Player({
       lastName: "Mustermann",
     });
     let err;
     try {
       await playerWithoutFirstName.validate();
-      console.log("Kein Fehler gefunden");
     } catch (error) {
       err = error;
-      console.log("FEHLER GEFANGEN:", err.errors ? "Ja" : "Nein");
     }
     // Prüfung: existiert überhaupt ein Fehler?
     expect(err.errors.firstName).toBeDefined();
     // Prüfung: existiert die korrekte Fehlermeldung
     expect(err.errors.firstName.message).toBe("Vorname ist erforderlich");
+  });
+  it("should fail if firstName is shorter than 2 characters", async () => {
+    const shortNamePlayer = new Player({
+      lastName: "Mustermann",
+      firstName: "A",
+    });
+    let err = null;
+    try {
+      await shortNamePlayer.validate();
+    } catch (error) {
+      err = error;
+      console.log("err", err);
+    }
+    expect(err).not.toBeNull();
+    expect(err.errors.firstName.kind).toBe("minlength");
+    expect(err.errors.firstName.message).toBe(
+      "Vorname muss mindestens 2 Zeichen haben",
+    );
+  });
+  it("should validate a correct player without errors", async () => {
+    const validPlayer = new Player({
+      firstName: "Andreas",
+      lastName: "Mustermann",
+      email: "andreas@poker.de",
+    });
+
+    await expect(validPlayer.validate()).resolves.toBeUndefined();
   });
 });
